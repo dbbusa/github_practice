95b75f0a4c2ae8e56d13281542cb49f9f9c659fe
Rutika Sorthiya <68726274+rksorthiya@users.noreply.github.com>
2022-03-02 19:20:48 +0530

    Add files via upload

---
+++ rutika-portfolio/practice/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    '*',
+    './component/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily:{
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
