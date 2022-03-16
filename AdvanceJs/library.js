ea57c7248f911b3f1de378deecb52847df547095
Arazu Kaneriya <akaneriya2228@outlook.com>
2022-03-16 12:43:51 +0530

    JAVASCRIPT ADVANCE ADD

---
+++ AdvanceJs/library.js
@@ -0,0 +1,13 @@
+// export let message = "Hello from main modules";
+// export function user(){
+//     console.log("hello world")
+// }
+// export class test{
+//     constructor(){
+//        console.log("Constructor Method")
+//     }
+// }
+
+// export default function(){
+//     console.log("hello from default function");
+// }
---
+++ AdvanceJs/main.js
@@ -0,0 +1,13 @@
+// import {message , user , test} from './library.js'
+// document.body.innerHTML = message;
+// console.log(message);
+// console.log(user());
+// let a = new test()
+
+// import {default as hello } from './library.js'
+// hello();
+
+// import {message,user,test} from './library.js'
+// document.body.innerText = message
+// console.log(user());
+// let a = new test()
